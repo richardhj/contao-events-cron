cd72930388c8a3d9bd9deaeecee214ed3120eeed
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-02-03 00:34:15 +0100

    Create bundle class and manager plugin

---
+++ src/CcaEventsCronBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Cron event for Contao Open Source CMS
+ * Copyright (C) 2013, 2014 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  (c) 2013, 2014 Contao Community Alliance
+ * @author         Tristan Lins <tristan.lins@bit3.de>
+ * @package        events-cron
+ * @license        LGPL
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\Contao\Events\Cron;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class CcaEventsCronBundle
+ */
+class CcaEventsCronBundle extends Bundle
+{
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Cron event for Contao Open Source CMS
+ * Copyright (C) 2013, 2014 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  (c) 2013, 2014 Contao Community Alliance
+ * @author         Tristan Lins <tristan.lins@bit3.de>
+ * @package        events-cron
+ * @license        LGPL
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\Contao\Events\Cron\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoCommunityAlliance\Contao\Events\Cron\CcaEventsCronBundle;
+
+/**
+ * Class Plugin
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @param ParserInterface $parser
+     *
+     * @return ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CcaEventsCronBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                    ]
+                )
+                ->setReplace(['events-cron']),
+        ];
+    }
+}
